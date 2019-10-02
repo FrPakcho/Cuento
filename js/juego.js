34edcb96d1898c058cefc98f87cd3ed2b409d2d7
FrPakcho <55297665+FrPakcho@users.noreply.github.com>
2019-10-02 13:19:48 -0400

    Update juego.js
    finalización del juego

--- js/juego.js
+++ js/juego.js
@@ -1,20 +1,19 @@
-var audio;
+var finDeJuego=0;
 /**
 * Función que se ejecuta al arrastrar el elemento. 
 **/
 function start(e) {
 	e.dataTransfer.effecAllowed = 'move'; // Define el efecto como mover (Es el por defecto)
 	e.dataTransfer.setData("Text", e.target.id); // Coje el elemento que se va a mover
-	e.target.style.opacity = '0.9'; 
+	e.target.style.opacity = '0.7'; 
 }
 
 /**
-* Función que se ejecuta se termina de arrastrar el elemento. 
+* Función que se ejecuta cuando se termina de arrastrar el elemento. 
 **/
 function end(e){
 	e.target.style.opacity = ''; // Restaura la opacidad del elemento			
-	e.dataTransfer.clearData("Data");
-	comprobarPuzzle();		
+	e.dataTransfer.clearData("Data");			
 }
 
 /**
@@ -41,31 +40,75 @@ function over(e) {
 function drop(e){
 	e.preventDefault(); // Evita que se ejecute la accion por defecto del elemento soltado.
 	var elementoArrastrado = e.dataTransfer.getData("Text");
-	e.target.appendChild(document.getElementById(elementoArrastrado)); // Coloca el elemento soltado sobre el elemento desde el que se llamo esta funcion
-	comprobarPuzzle();
+	// Coloca el elemento soltado sobre el elemento desde el que se llamo esta funcion
+	e.target.appendChild(document.getElementById(elementoArrastrado));
+	var valor_Destino=e.target.id;
+	comprobarPuzzle(elementoArrastrado,valor_Destino);
+}
+/**
+ * Reproducir Audio
+ */
+function audio(e){
+	var reproducir=new Audio();
+	reproducir.src=e;
+	reproducir.play();
 }
 
-function comprobarPuzzle(){
-	console.log(document.getElementById('pieza1').parentNode.id);
-	/**
-	if(document.getElementById('pieza1').parentNode.id=='uno'){
-		reproducir('taza');
-		console.log("taza");
-	}else if(document.getElementById('pieza2').parentNode.id=='dos'){
-		reproducir('casa');
-		console.log("casa");
-	}else if(document.getElementById('pieza3').parentNode.id=='tres'){
-		reproducir('mesa');
-		console.log("mesa");
-	}else if(document.getElementById('pieza4').parentNode.id=='cuatro'){
-		reproducir('vitrina');
-		console.log("vitrina");
-	}**/
-}
+function comprobarPuzzle(piezaOrigen,piezaDestino){
 
-function reproducir(nombre){
-	audio=new Audio(("./audio/".concat(nombre)).concat(".mp3"));
-	audio.play();
+	if(piezaOrigen=="pieza1" && piezaDestino=="uno")
+	{
+		finDeJuego=finDeJuego+1;
+		if(finDeJuego<4){
+			audio("audio/correcto.mp3");
+			document.getElementById("calificar").innerHTML='<img src="imagen/juego/correcto.gif"/>';
+		}
+		else{
+			audio("audio/finDeJuego.mp3");
+			document.getElementById("calificar").innerHTML='<img src="imagen/juego/felicidades.gif"/>';
+		}
+	}
+	else if(piezaOrigen=="pieza2" && piezaDestino=="dos")
+	{
+		finDeJuego=finDeJuego+1;
+		if(finDeJuego<4){
+			audio("audio/correcto.mp3");
+			document.getElementById("calificar").innerHTML='<img src="imagen/juego/correcto.gif"/>';
+		}
+		else{
+			audio("audio/finDeJuego.mp3");
+			document.getElementById("calificar").innerHTML='<img src="imagen/juego/felicidades.gif"/>';
+		}
+	}
+	else if(piezaOrigen=="pieza3" && piezaDestino=="tres")
+	{
+		finDeJuego=finDeJuego+1;
+		if(finDeJuego<4){
+			audio("audio/correcto.mp3");
+			document.getElementById("calificar").innerHTML='<img src="imagen/juego/correcto.gif"/>';
+		}
+		else{
+			audio("audio/finDeJuego.mp3");
+			document.getElementById("calificar").innerHTML='<img src="imagen/juego/felicidades.gif"/>';
+		}
+	}
+	else if(piezaOrigen=="pieza4" && piezaDestino=="cuatro")
+	{
+		finDeJuego=finDeJuego+1;
+		if(finDeJuego<4){
+			audio("audio/correcto.mp3");
+			document.getElementById("calificar").innerHTML='<img src="imagen/juego/correcto.gif"/>';
+		}
+		else{
+			audio("audio/finDeJuego.mp3");
+			document.getElementById("calificar").innerHTML='<img src="imagen/juego/felicidades.gif"/>';
+		}
+	}
+	else
+	{
+		audio("audio/incorrecto.mp3");
+		document.getElementById("calificar").innerHTML='<img src="imagen/juego/incorrecto.gif"/>';
+	}
 }
 
 /**
@@ -76,7 +119,7 @@ function comprobarnavegador() {
 		(navigator.userAgent.toLowerCase().indexOf('msie ') > -1) || 
 		((navigator.userAgent.toLowerCase().indexOf('safari') > -1) && (navigator.userAgent.toLowerCase().indexOf('chrome') == -1)))
 	{
-		alert("Tu navegador no soporta correctamente las funciones Drag & Drop de HTML5. Prueba con otro navegador.");
+		alert("Tu navegador no soporta correctamente las funciones requeridas para ejecutar el Juego. Prueba con otro navegador.");
 	}
 
 }
